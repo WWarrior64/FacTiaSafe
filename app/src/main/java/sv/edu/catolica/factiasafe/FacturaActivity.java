5068fb28543b4925ef4edd6152c3e57f0bd54521
WWarrior64 <115961340+WWarrior64@users.noreply.github.com>
2025-10-15 09:53:21 -0600

    Inicio del proyecto FacTia Safe incluyendo la primera vista de facturas aun no funcional

---
+++ app/src/main/java/sv/edu/catolica/factiasafe/FacturaActivity.java
@@ -0,0 +1,67 @@
+package sv.edu.catolica.factiasafe;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class FacturaActivity extends AppCompatActivity {
+
+    private FloatingActionButton mainFab;
+    private LinearLayout fabMenuContainer;
+    private boolean isMenuOpen = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_factura);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        mainFab = findViewById(R.id.main_fab);
+        fabMenuContainer = findViewById(R.id.fab_menu_container);
+        fabMenuContainer.setVisibility(View.GONE);
+
+        mainFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleFabMenu();
+            }
+        });
+    }
+
+    private void toggleFabMenu() {
+        if (isMenuOpen) {
+            // Si el menú está abierto: OCULTAR
+
+            // Oculta el contenedor del menú
+            fabMenuContainer.setVisibility(View.GONE);
+
+            // Cambiar el ícono a '+' (ic_add)
+            mainFab.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_add));
+
+            isMenuOpen = false;
+        } else {
+            // Si el menú está cerrado: MOSTRAR
+
+            // Muestra el contenedor del menú
+            fabMenuContainer.setVisibility(View.VISIBLE);
+
+            // Cambiar el ícono a 'X' (ic_close)
+            mainFab.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_close));
+
+            isMenuOpen = true;
+        }
+    }
+}
