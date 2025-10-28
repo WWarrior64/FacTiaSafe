a9c5998e3ad147940e25d05069ba84af79121f29
saraascencio <sara.ascencio1@catolica.edu.sv>
2025-10-27 22:17:43 -0600

    Se añadió EdgeToEdge para la corrección sugerida el día de hoy

--- app/src/main/java/sv/edu/catolica/factiasafe/EscanearActivity.java
+++ app/src/main/java/sv/edu/catolica/factiasafe/EscanearActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -21,7 +25,14 @@ public class EscanearActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_escanear);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         botonImagen = findViewById(R.id.boton_imagen);
         botonTexto = findViewById(R.id.boton_texto);
--- app/src/main/java/sv/edu/catolica/factiasafe/PrivacidadSeguridadActivity.java
+++ app/src/main/java/sv/edu/catolica/factiasafe/PrivacidadSeguridadActivity.java
@@ -6,8 +6,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
 
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -20,7 +25,15 @@ public class PrivacidadSeguridadActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_privacidad_seguridad);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
 
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
